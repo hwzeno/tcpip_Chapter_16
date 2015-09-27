3ceef6441ce119936a2f7bf1e8d1632d11a2dc28
drzeno <drzeno@yeah.net>
2015-09-27 13:31:31 +0800

    非半关闭状态，而是完全关闭

---
+++ sep_clnt.c
@@ -0,0 +1,52 @@
+/*********************************
+    > File Name: sep_clnt.c
+    > Author: Zeno
+    > Mail:340562424@qq.com 
+    > Created Time: 2015年09月27日 星期日 13时08分47秒
+ ********************************/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#define BUF_SIZE 1024
+
+int main(int argc, char *argv[])
+{
+	int sock;
+	char buf[BUF_SIZE];
+	struct sockaddr_in serv_addr;
+
+	FILE *readfp;
+	FILE *writefp;
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	readfp = fdopen(sock, "r");
+	writefp = fdopen(sock, "w");
+
+	connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	readfp = fdopen(sock, "r");
+	writefp = fdopen(sock, "w");
+
+	while(1)
+	{
+		if(fgets(buf, sizeof(buf), readfp) == NULL)
+			break;
+		fputs(buf, stdout);
+		fflush(stdout);
+	}
+
+	fputs("FROM CLIENT: Thank oU\n", writefp);
+	fflush(writefp);
+	fclose(writefp);
+	fclose(readfp);
+	return 0;
+
+}
